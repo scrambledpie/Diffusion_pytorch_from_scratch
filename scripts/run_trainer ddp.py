53dcdb3acdb2b506bb08deea89d8ea144f387ed1
Michael Pearce <scrambledpie@gmail.com>
2024-09-13 09:06:37 +0000

    refactor out common training functions

--- scripts/run_trainer ddp.py
+++ scripts/run_trainer ddp.py
@@ -1,17 +1,13 @@
-import time
-
 import torch
 import torch.optim as optim
-import torch.nn.functional as F
+from tensorboardX import SummaryWriter
 from torch.utils.data import DataLoader
 
-from diffusion.noise_schedule import apply_noise
 from diffusion.unet.unet import UNet
 from dataset.datasets import Flowers, CelebA, CelebA10k
+from diffusion.train import train_model
 
 from folders import make_new_folders
-
-from tensorboardX import SummaryWriter
 
 # DDP Boiler Plate Start
 import torch.multiprocessing as mp
@@ -34,67 +30,6 @@ def ddp_setup(rank, world_size):
 # DDP Boiler Plate End
 
 
-def train_model(
-    model: torch.nn.Module,
-    optimizer: torch.optim.AdamW,
-    dataloader: DataLoader,
-    epochs:int = 10,
-    rank:int=0,
-):
-    if rank == 0:
-        CHECKPOINT_DIR, LOG_DIR = make_new_folders()
-        writer = SummaryWriter(log_dir=LOG_DIR)
-
-    model.train()
-    epoch_start = time.time()
-
-    model = model.to(rank)
-    model = DDP(model, device_ids=[rank])
-
-    iter_count = 0
-    for epoch in range(epochs):
-        epoch_start = time.time()
-        epoch_loss = 0
-        epoch_iters = 0
-        for i, x_input in enumerate(dataloader):
-            batch_start = time.time()
-
-            # clean image (batch, channels, height, width)
-            x_input = x_input.to(rank)
-
-            # noisy image, only noise, and noise standard devitaions
-            x_diffused, x_noise, noise_sd = apply_noise(x_input)
-
-            # forward
-            optimizer.zero_grad()
-            x_noise_predicted = model(x_diffused, noise_sd**2)
-            mse_loss = F.mse_loss(x_noise_predicted, x_noise)
-
-            # backward
-            mse_loss.backward()
-            optimizer.step()
-
-            if rank == 0:
-                epoch_loss += mse_loss
-                epoch_iters += 1
-                batch_time = time.time() - batch_start
-                print(f"{epoch}. {i}: {mse_loss:.3f} {batch_time:.3f} seconds")
-                writer.add_scalar("mse_loss", mse_loss, iter_count)
-                iter_count += 1
-
-        if rank == 0:
-            epoch_time = time.time() - epoch_start
-            checkpoint_file = CHECKPOINT_DIR / f"{epoch} epoch.pt"
-            torch.save(model.module.state_dict(), f=checkpoint_file)
-
-            print(
-                f"{epoch}: epoch time: {epoch_time:.4} seconds\n"
-                f"Saved model: {checkpoint_file}\n"
-            )
-            writer.add_scalar("epoch time", epoch_time, epoch)
-            writer.add_scalar("epoch loss", epoch_loss / epoch_iters, epoch)
-
-
 def main(rank:int, world_size:int=2):
     ddp_setup(rank=rank, world_size=world_size)
     dataset = CelebA10k()
@@ -106,18 +41,34 @@ def main(rank:int, world_size:int=2):
         sampler=DistributedSampler(dataset)
     )
     model = UNet()
+    model.train()
+    model = DDP(model, device_ids=[rank])
 
     print(f"Rank: {rank}")
 
     optimizer = optim.AdamW(model.parameters(), lr=0.0001)
-    train_model(
+
+    # training setting for all nodes
+    train_model_kwargs = dict(
         model=model,
         dataloader=dataloader,
         optimizer=optimizer,
         epochs=20000,
-        rank=rank,
+        verbose=False,
     )
 
+    if rank == 0:
+        # extra logging settings just for 0th node
+        checkpoint_dir, log_dir = make_new_folders()
+        train_model_kwargs.update(
+            dict(
+                checkpoint_dir=checkpoint_dir,
+                verbose = True,
+                tensorboard_writer=SummaryWriter(logdir=log_dir),
+            )
+        )
+
+    train_model(**train_model_kwargs)
     destroy_process_group()
 
 
--- scripts/run_trainer.py
+++ scripts/run_trainer.py
@@ -1,65 +1,20 @@
-import time
-
-import torch
 import torch.optim as optim
-import torch.nn.functional as F
+from tensorboardX import SummaryWriter
 from torch.utils.data import DataLoader
 
-from diffusion.noise_schedule import apply_noise
+
 from diffusion.unet.unet import UNet
 from dataset.datasets import Flowers, CelebA, CelebA10k
+from diffusion.train import train_model
 
 from folders import make_new_folders
 
 
-CHECKPOINT_DIR, LOG_DIR = make_new_folders()
-
-
-def train_model(
-    *,
-    model: torch.nn.Module,
-    optimizer: torch.optim.AdamW,
-    dataloader: DataLoader,
-    epochs:int = 10,
-):
-    model.train()
-
-    epoch_start = time.time()
-
-    for epoch in range(epochs):
-        epoch_start = time.time()
-        for i, x_input in enumerate(dataloader):
-            batch_start = time.time()
-
-            # clean images (batch, channels, height, width)
-            x_input = x_input.to(model.device)
-
-            # add noise to the images
-            x_diffused, x_noise, noise_sd = apply_noise(x_input)
-
-            # forward
-            optimizer.zero_grad()
-            x_noise_predicted = model(x_diffused, noise_sd**2)
-            mse_loss = F.mse_loss(x_noise_predicted, x_noise)
-
-            # backward
-            mse_loss.backward()
-            optimizer.step()
-
-            batch_time = time.time() - batch_start
-            print(f"{epoch}. {i}: {mse_loss:.3f} {batch_time:.3f} seconds")
-
-        # Track metrics, save checkpoint
-        epoch_time = time.time() - epoch_start
-        checkpoint_file = CHECKPOINT_DIR / f"{epoch} epoch.pt"
-        torch.save(model.state_dict(), f=checkpoint_file)
-        print(
-            f"{epoch}: epoch time: {epoch_time:.4} seconds\n"
-            f"Saved model: {checkpoint_file}\n"
-        )
 
 
 def main():
+    checkpoint_dir, log_dir = make_new_folders()
+
     dataset = CelebA10k()
     dataloader = DataLoader(
         dataset,
@@ -69,14 +24,15 @@ def main():
     )
 
     model = UNet(device="cuda")
-    model.train()
     optimizer = optim.AdamW(model.parameters(), lr=0.0001)
 
     train_model(
         model=model,
         optimizer=optimizer,
         dataloader=dataloader,
-        epochs=10000,
+        checkpoint_dir=checkpoint_dir,
+        epochs=3,
+        tensorboard_writer=SummaryWriter(logdir=log_dir)
     )
 
 
